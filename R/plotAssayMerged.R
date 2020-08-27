03092a825999636dc6fb22ab384062d4e4a3e05c
Steve Ped <stephen.pederson@adelaide.edu.au>
2020-08-27 11:22:35 +0930

    Added dots to plotAssayMerged

--- R/plotAssayMerged.R
+++ R/plotAssayMerged.R
@@ -11,13 +11,10 @@
 #'
 #' @param x An IncucyteExperiment object
 #' @param assay The assay to be visualised
-#' @param point_aes Plotting aesthetics for points.
-#' Any aesthetics must refer to columns in the colData slot of x.
-#' Any aesthetics provided here will become the grouping categories for
-#' calculating and plotting the mean+se of each category.
-#' Must be specified.
-#' @param line_aes Plotting aesthetics for lines.
-#' If not provided will default to the same as point_aes
+#' @param ... Passed to \code{aes_string()}. If no colour, linetype or other
+#' grouping category is provided, the plot will simply be a summary of all
+#' points in all wells. Any provided categories must correspond to columns
+#' contained in the colData slot of \code{x}
 #' @param trans The transformation for the y-axis
 #' @param pointsize The size of points to be drawn
 #'
@@ -31,14 +28,14 @@
 setGeneric(
     name = "plotAssayMerged",
     def = function(
-        x, assay, point_aes, line_aes, trans = "log10", pointsize = 1/3
+        x, assay, ..., trans = "log10", pointsize = 1/3
     ){
         standardGeneric("plotAssayMerged")
     }
 )
 #' @rdname plotAssayMerged-methods
 #' @export
-setMethod("plotAssayMerged", signature = "ANY", function(x){
+setMethod("plotAssayMerged", signature = "ANY", function(x, ...){
     .errNotImp(x)
 }
 )
@@ -48,36 +45,24 @@ setMethod(
     f = "plotAssayMerged",
     signature = "IncucyteExperiment",
     definition = function(
-        x, assay, point_aes, line_aes, trans = "log10", pointsize = 1/3
+        x, assay, ..., trans = "log10", pointsize = 1/3
     ){
-        ## Check the aesthetics
-        if (missing(point_aes)) {
-            msg <- paste0(
-                "Plotting aesthetics must be supplied for points.\n",
-                "These must correspond to columns in the colData slot."
-            )
-            stop(msg)
-        }
-        if (missing(line_aes)) line_aes <- point_aes
-        stopifnot(.checkAes(point_aes, x))
-        stopifnot(.checkAes(line_aes, x))
+
         ## Check the assays
         if (length(assay) > 1) {
             msg <- "Too many assays. Only the first will be plotted."
             warning(msg)
             assay <- assay[[1]]
         }
+
         ## Make the plot
         df <- assay2long(x, assay)
-        ## Maybe we should place the dots here for other aesthetics & have
-        ## them all inherited
-        ggplot(df, aes_string("Elapsed", assay)) +
+        ggplot(df, aes_string("Elapsed", assay, ...)) +
             stat_summary(
-                point_aes,
                 fun.data = mean_se, geom = "pointrange",
                 size = pointsize
             ) +
-            stat_summary(line_aes, fun = mean, geom = "line") +
+            stat_summary(fun = mean, geom = "line") +
             scale_y_continuous(trans = trans)
 
     }
